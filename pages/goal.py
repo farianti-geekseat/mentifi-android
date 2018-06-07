49f1e56af6a0cf3c17d407da1bbbb2b5d96c93d1
users\Geekseat <alamta66@gmail.com>
2018-06-07 09:31:19 +0700

    update 070618

--- pages/goal.py
+++ pages/goal.py
@@ -17,6 +17,8 @@ class Goal(Page):
     edit_button = (By.XPATH,"//*[@id='ib_action_more' and (./preceding-sibling::* | ./following-sibling::*)[@text='hard']]")
     set_goal = (By.ID,'com.hub.mentifi:id/input_goal')
     probability = (By.ID,'com.hub.mentifi:id/spinner_probability')
+    ok_button = (By.XPATH,"//*[@text='OK']")
+    prob = (By.XPATH,"//*[@text='Impossible']")
     save = (By.XPATH,"//*[@text='Save']")
     def click_goal(self):
         self.find_element(self.goal).click()
@@ -27,7 +29,7 @@ class Goal(Page):
         for i in test:
             j = i.text
             print(j)
-
+#//*[@class='android.widget.FrameLayout']
     def edit_goal(self,alias,title):
         edit_parent = self.driver.find_element(By.ID,'com.hub.mentifi:id/recycler_view_goal')
         edit = edit_parent.find_elements(By.CLASS_NAME, 'android.widget.ImageButton')
@@ -42,16 +44,21 @@ class Goal(Page):
             WebDriverWait(self.driver, 10).until(ec.presence_of_element_located(self.set_goal))
         except TimeoutException:
             print("element not ready")
+
         self.find_element(self.set_goal).clear()
         self.find_element(self.set_goal).send_keys(title)
-        # self.find_element(self.probability).click()
-        # time.sleep(2)
-        # test_parent = self.driver.find_element(By.XPATH,"//*[@class='android.widget.ListView']")
-        # tests = test_parent.find_elements(By.CLASS_NAME, 'android.widget.TextView')
-        # tests[4].click()
+        self.find_element(self.probability).click()
+        # test_parent = self.driver.find_element(By.ID,'com.hub.mentifi:id/spinner_probability')
+        # tests = test_parent.find_elements(By.XPATH,'android.widget.ListView')
+        # print(tests)
+        a = self.find_element(By.ID,'android:id/text1')
+        print(a)
+        # tests.click
         self.find_element(self.save).click()
-
-#android.widget.ListView
+#By.XPATH,"//*[@class='android.widget.FrameLayout']"
+#android.widget.ScrollView
+#android:id/text1 id,text1
+#//*[@class='android.widget.ListView']
     def remove_goal(self, alias):
         edit_parent = self.driver.find_element(By.ID, 'com.hub.mentifi:id/recycler_view_goal')
         edit = edit_parent.find_elements(By.CLASS_NAME, 'android.widget.ImageButton')
@@ -61,4 +68,5 @@ class Goal(Page):
         test_parent = self.driver.find_element(By.XPATH,"//*[@class='android.widget.FrameLayout' and ./*[@class='android.widget.ListView']]")
         test = test_parent.find_elements(By.CLASS_NAME, 'android.widget.LinearLayout')
         test[2].click()
+        self.find_element(self.ok_button).click()
         print('goal removed')
