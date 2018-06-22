8b4aac2d0cc7521bc9e6712b98bd961476d48fc2
users\Geekseat <alamta66@gmail.com>
2018-06-22 16:10:18 +0700

    update220618

--- pages/connection_dashboard.py
+++ pages/connection_dashboard.py
@@ -3,6 +3,7 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as ec
 from selenium.common.exceptions import TimeoutException, NoSuchElementException
 from pages import Page
+import time
 
 import pytest
 
@@ -16,6 +17,8 @@ class Connection(Page):
     view_connect =(By.XPATH,"//*[@id='recycler_view' and @width>0]")
     add_mentor =(By.XPATH,"//*[@id='floating_action']")
     con_dash =(By.ID,'com.hub.mentifi:id/tab_network')
+    list_profile = (By.ID,'com.hub.mentifi:id/recycler_view')
+    yes_button = (By.ID,'com.hub.mentifi:id/btn_positive_accept')
 
     def click_connection(self):
         self.find_element(self.con_dash).click()
@@ -46,6 +49,47 @@ class Connection(Page):
         for i in test:
             n = i.text
             print(n)
+    def view_profile(self,alias):
+        test_parent = self.driver.find_element(By.ID, 'com.hub.mentifi:id/viewpager')
+        test = test_parent.find_elements(By.CLASS_NAME, 'android.widget.ImageButton')
+        test[alias].click()
+        time.sleep(2)
+        profile = self.driver.find_elements(By.ID, 'android:id/title')
+        profile[0].click()
+        time.sleep(5)
+        test_parent2 = self.driver.find_element(By.ID, 'com.hub.mentifi:id/layout_header')
+        test2 = self.driver.find_elements(By.CLASS_NAME, 'android.widget.TextView')
+        for i in test:
+            z = i.text
+            print(z)
 
+    def pending_view_profile(self,alias):
+        test_parent = self.driver.find_element(By.ID, 'com.hub.mentifi:id/viewpager')
+        test = test_parent.find_elements(By.CLASS_NAME, 'android.widget.ImageButton')
+        test[alias].click()
+        time.sleep(2)
+        # test_profile = self.driver.find_element(By.XPATH, "//*[@class='android.widget.FrameLayout' and ./*[@class='android.widget.ListView']]")
+        profile = self.driver.find_elements(By.ID, 'android:id/title')
+        profile[0].click()
+        time.sleep(5)
+        # profile[0].click()
+        test_parent2 = self.driver.find_element(By.ID, 'com.hub.mentifi:id/layout_header')
+        test2 = self.driver.find_elements(By.CLASS_NAME, 'android.widget.TextView')
+        for i in test:
+            g = i.text
+            print(g)
+#com.hub.mentifi:id/collapsing_toolbar
+    #com.hub.mentifi:id/layout_header
+    def pending_remove_connection(self,alias):
+        test_parent = self.driver.find_element(By.ID, 'com.hub.mentifi:id/viewpager')
+        test = test_parent.find_elements(By.CLASS_NAME, 'android.widget.ImageButton')
+        test[alias].click()
+        time.sleep(2)
+        profile = self.driver.find_elements(By.ID, 'android:id/title')
+        profile[2].click()
+        self.find_element(self.yes_button).click()
+        print('connection removed')
+
+#android.widget.RelativeLayout
     def click_search(self):
         self.find_element(self.search).click()
--- testcase/test_connection.py
+++ testcase/test_connection.py
@@ -6,6 +6,7 @@ root = Path(__file__).parents[1]   #get the root directory
 root_model = str(root)
 sys.path.append(root_model)
 import pages
+import time
 from connection import Connection
 
 directory = '%s/' % os.getcwd()
@@ -14,16 +15,40 @@ driver = Connection.driver
 
 connection_dash = pages.Connection()
 login = pages.Login()
-
+password = 'ZXasqw12'
+mentor = "gamns08+yogya2@gmail.com"
+mentee = "gamns08+yogya3@gmail.com"
 class TestConnection():
 
-    def test_connection(self):
+    # def test_connection_connected_view(self):
+    #     login.verified_all_element()
+    #     login.input_email(mentee)
+    #     login.input_password(password)
+    #     login.tap_sign_in()
+    #     login.select_company(0)
+    #     connection_dash.click_connection()
+    #     connection_dash.click_connected()
+    #     time.sleep(2)
+    #     connection_dash.view_profile(1)
+
+    def test_connection_pending_view(self):
         login.verified_all_element()
-        login.input_email("gamns08+yogya2@gmail.com")
-        login.input_password("ZXasqw12")
+        login.input_email(mentee)
+        login.input_password(password)
         login.tap_sign_in()
-        login.select_company()
+        login.select_company(0)
         connection_dash.click_connection()
-        connection_dash.click_connected()
         connection_dash.click_pending()
-        connection_dash.click_request()
+        time.sleep(2)
+        connection_dash.pending_view_profile(0)
+
+    # def test_connection_pending_remove_connection(self):
+    #     login.verified_all_element()
+    #     login.input_email(mentee)
+    #     login.input_password(password)
+    #     login.tap_sign_in()
+    #     login.select_company(0)
+    #     connection_dash.click_connection()
+    #     connection_dash.click_pending()
+    #     time.sleep(2)
+    #     connection_dash.pending_remove_connection(0)
